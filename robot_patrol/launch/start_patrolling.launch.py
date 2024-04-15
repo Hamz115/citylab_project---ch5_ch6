573816c1ed7ed8748d0a09d4af1c4728dd4a6185
Hamza Feroze <hamzaferoze115@gmail.com>
2024-04-15 14:34:13 +0000

    Added some changes to the launch file

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='your_package_name',
+            package='robot_patrol',
             executable='patrol',
             name='patrol'
         )
