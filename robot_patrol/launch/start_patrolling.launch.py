9b5c696cc5c7b7d472cc9c4e127b78a90d9d297b
Hamza Feroze <hamzaferoze115@gmail.com>
2024-05-05 14:28:09 +0000

    Created srv message GetDirection in the srv folder

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -5,25 +5,8 @@ def generate_launch_description():
       return LaunchDescription([
           Node(
               package='robot_patrol',
-              executable='robot_patrol',
+              executable='robot_patrol_node',
               output='screen',
         )
       ])
 
-# from launch import LaunchDescription
-# from launch_ros.actions import Node
-
-# def generate_launch_description():
-
-#     patrol_node = Node(
-#         package='robot_patrol',
-#         executable='patrol_node',
-#         output='screen',
-#         remappings=[
-#             ('laser_scan', '/laser_scan')
-#         ]
-#     )
-
-#     return LaunchDescription([
-#         patrol_node
-#     ])
